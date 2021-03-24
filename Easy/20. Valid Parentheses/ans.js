8dc4447246c43be535321e96fb6c895671d9599b
prachuryanath <prachuryanath00@gmail.com>
2021-03-24 13:21:54 +0530

    Easy- 20

---
+++ Easy/20. Valid Parentheses/ans.js
@@ -0,0 +1,12 @@
+var isValid = function(s) {
+    let preS = ''
+    while(s !== preS){
+        preS = s
+        s = s.split('()').join('')
+        s = s.split('[]').join('')
+        s = s.split('{}').join('') 
+    }
+    if(!s)
+        return true;
+    return false;
+};
