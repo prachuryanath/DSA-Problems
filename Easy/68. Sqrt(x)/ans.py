70ce59c36019d33b92e2e943e4450ec15072809b
prachuryanath <prachuryanath00@gmail.com>
2021-03-29 22:39:32 +0530

    Easy- 68

---
+++ Easy/68. Sqrt(x)/ans.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(sqrt(x))
+
+
+# Binary Search Implementation :
+
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2: return x
+        result = 0 
+        left = 1
+        right = x/2
+        while left <= right:
+            mid = math.floor((left+right)/2)
+            if mid * mid > x:
+                right = mid - 1
+            else:
+                result = mid
+                left = mid + 1
+        return result
