03c37be638e933685fcd314863561a20800a0d10
prachuryanath <prachuryanath00@gmail.com>
2021-03-31 20:24:53 +0530

    Easy- 100

---
+++ Easy/100. Same Tree/ans.js
@@ -0,0 +1,7 @@
+var isSameTree = function(p, q) {
+    // the same if both nodes are null
+    if (!p && !q) return true;
+    // not the same if either of the nodes is null or the values are different
+    if ((!p && q) || (p && !q) || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
